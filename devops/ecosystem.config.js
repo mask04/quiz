b7f5dd74bb3734b2ef57f609295d878129c6a16c
Emma Ahouangonou <emma.ahouangonou@epitech.eu>
2026-08-28 18:06:39 +0100

    fix : switch to fork mode to fix crash"

--- devops/ecosystem.config.js
+++ devops/ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'quiz-api',
     script: 'server.js',
     cwd: '/var/www/RP/quiz',
+    exec_mode: 'fork',
     instances: 1,
     autorestart: true,
     watch: false,
